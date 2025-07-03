c767676c3f5c26f7cb3c06bb2988ea06b2fc1ea7
kwcLakshitha <your-email@example.com>
2025-07-03 10:25:20 +0530

    Rest controllers

---
+++ String Framework/09_Rest_Controllers/src/main/java/edu/ijse/WebAppInitializer.java
@@ -0,0 +1,27 @@
+package edu.ijse;
+
+import edu.ijse.config.WebAppConfig;
+import edu.ijse.config.WebRootConfig;
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?> @Nullable [] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?> @Nullable [] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
---
+++ String Framework/09_Rest_Controllers/src/main/java/edu/ijse/config/WebRootConfig.java
@@ -0,0 +1,4 @@
+package edu.ijse.config;
+
+public class WebRootConfig {
+}
